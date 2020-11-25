7cb033864192bca78c36412cadbc2031d898cf6d
theinzawwin <theinzawwin92@gmail.com>
2020-11-25 22:01:20 +0630

    ad all

---
+++ src/com/best/model/Category.java
@@ -0,0 +1,25 @@
+package com.best.model;
+
+public class Category {
+
+	private Integer id;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+	public Category(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+}
